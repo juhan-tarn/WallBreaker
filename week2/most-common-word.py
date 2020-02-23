16e8c0c740b4c70027e293dd1a730b919f7c0a8b
TarnJu-Han <juhan1960@gmail.com>
2020-02-23 16:32:00 -0500

    added most common word

---
+++ week2/most-common-word.py
@@ -0,0 +1,27 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        new_paragraph = paragraph.lower().split()
+        
+        ban_set = set(banned)
+        word_count, max_word, max_count = {}, '', 0
+        
+        for word in new_paragraph:
+            if word in ban_set:
+                continue
+            word_count[word] = word_count.get(word, 0) + 1
+            if word_count[word] > max_count:
+                max_word, max_count = word, word_count[word]
+            
+        return max_word
+
+    def mostCommonWord_II(self, paragraph: str, banned: List[str]) -> str:    
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        words = paragraph.lower().split()
+        count = collections.Counter(words)
+        for word in banned:
+            del count[word]
+        return count.most_common(1)[0][0]
+        
